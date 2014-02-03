59e0a7394fa5f0a46627b9865403ea6b479f02cb
tkendalls <director@mathplusacademy.com>
2014-02-03 13:41:05 -0500

    admin page info

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
 
   def home
+    @registrations = Registration.all
     @months_registrations = Registration.where("created_at >= ?", Time.zone.now.beginning_of_month)
     @todays_registrations = Registration.where("created_at >= ?", Time.zone.now.beginning_of_day)
+    @camp_off_reg_count = 0
+
+    @registrations.each do |reg|
+      @camp_off_reg_count += reg.camp_offerings.count
+    end
   end
 end
