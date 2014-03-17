30e258f13a8d0802d06ec2be53f7152f2363b42c
tkendalls <director@mathplusacademy.com>
2014-03-17 12:15:15 -0400

    add open spots to powell calendar

--- app/helpers/camp_offerings_helper.rb
+++ app/helpers/camp_offerings_helper.rb
@@ -1,2 +1,13 @@
 module CampOfferingsHelper
+  def open_spots(co)
+    spots = co.open_spots.to_i
+    case spots
+    when 1
+      "#{co.open_spots} spot"
+    when 2..4
+      "#{co.open_spots} spots"
+    else
+      ""
+    end
+  end
 end
