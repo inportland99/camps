7b67e8c956e8f81374df760915b70d78bb6d6bdc
tkendalls <director@mathplusacademy.com>
2014-05-15 20:59:31 -0400

    mime type pdf and strip key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,9 @@
-Stripe.api_key = "sk_test_GmlILZBDjmVQdF4ywsG6rjHG"
-STRIPE_PUBLIC_KEY = "pk_test_GaO03iFegnH0RU0MJwqdRuBe"
+
 
 if Rails.env.production?
   Stripe.api_key = ENV['STRIPE_API_KEY']
   STRIPE_PUBLIC_KEY = ENV['STRIPE_PUB_KEY']
+else
+  Stripe.api_key = "sk_test_GmlILZBDjmVQdF4ywsG6rjHG"
+  STRIPE_PUBLIC_KEY = "pk_test_GaO03iFegnH0RU0MJwqdRuBe"
 end
