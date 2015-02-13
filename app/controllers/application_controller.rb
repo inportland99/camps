f7fbebb4f5c539ed6dda003a7c097c45f036bf6f
tkendalls <director@mathplusacademy.com>
2015-02-13 15:03:18 -0500

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,5 @@ require "application_responder"
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
-  force_ssl
   protect_from_forgery
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,7 @@
 class RegistrationsController < ApplicationController
   # GET /registrations
   # GET /registrations.json
+  force_ssl
   before_filter :authenticate_user!, :except => [:new, :create]
 
   def index
