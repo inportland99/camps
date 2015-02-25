642b0902c172f8f4d856e9981982b9e2428f1288
tkendalls <director@mathplusacademy.com>
2015-02-24 22:04:19 -0500

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
       @registrations = Registration.where(year: 1)
       @months_registrations = Registration.where("created_at >= ? AND year=?", Time.now.beginning_of_month, 1)
       @months_registrations_last_year = Registration.where("created_at >= ? AND created_at <= ? AND year = ?", (Time.now - 1.year).beginning_of_month, (Time.now - 1.year).end_of_month, 0)
-      @percent_different = ((@months_registrations.count - @months_registrations_last_year.count).to_f/@months_registrations_last_year.count.to_f)
+      @percent_different = ((@months_registrations.to_a.sum{ |reg| reg.camp_offerings.count } - @months_registrations_last_year.to_a.sum{ |reg| reg.camp_offerings.count }).to_f/(@months_registrations_last_year.to_a.sum{ |reg| reg.camp_offerings.count }).to_f).round(3)
       @todays_registrations = Registration.where("created_at > ? AND year =?", Time.now.beginning_of_day, 1)
       @camp_interest = CampInterest.new
       @camp_off_reg_count = 0
