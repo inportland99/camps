f21471e080130e91f5231a22ffb99bc108482df8
tkendalls <director@mathplusacademy.com>
2017-01-23 21:35:44 -0500

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
 
   def bootstrap_class_for flash_type
-    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym]
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
   end
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
-              concat content_tag(:button, "&times".html_safe, class: "close", data: { dismiss: 'alert' })
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}") do
+              concat content_tag(:button, "&times;".html_safe, class: "close", data: { dismiss: 'alert' })
               concat message
             end)
     end
