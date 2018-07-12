1c1c0abe555793f5f278c3b8816afb35b3ee0ed8
Raj Shah <drrajshah@me.com>
2018-07-12 14:41:10 -0400

    added week 9 to 2018 camps

--- app/models/camp_offering.rb
+++ app/models/camp_offering.rb
@@ -44,10 +44,10 @@ class CampOffering < ActiveRecord::Base
                                     :start => Date.new(2018,7,30),
                                     :end => Date.new(2018,8,3)
                             },
-                            # 9 => {
-                            #         :start => Date.new(2018,8,6),
-                            #         :end => Date.new(2018,8,10)
-                            # }
+                            9 => {
+                                    :start => Date.new(2018,8,6),
+                                    :end => Date.new(2018,8,10)
+                            }
     }
   elsif Rails.env.production?
     OFFERING_WEEKS = {
