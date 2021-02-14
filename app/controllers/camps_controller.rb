eb45c269c06ed94890f9f68bcfd1c0229d92e3b1
Raj Shah <drrajshah@me.com>
2021-02-14 17:17:37 -0500

    Add active column to camps

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -105,6 +105,6 @@ class CampsController < ApplicationController
     # params.require(:person).permit(:name, :age)
     # Also, you can specialize this method with per-user checking of permissible attributes.
     def camp_params
-      params.require(:camp).permit(:age, :capacity, :description, :price, :title, :show_description, :girls_only, :video_url, :online)
+      params.require(:camp).permit(:age, :capacity, :description, :price, :title, :show_description, :girls_only, :video_url, :online, :active)
     end
 end
